d7ce191f05692dacf6cc44c2f48645d3b11ec9c6
Gao Kang <gaokang@Gaos-MacBook-Pro.local>
2021-12-10 10:40:24 +1300

    home page setup

---
+++ App/screens/WelcomeScreen.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Image, ImageBackground, StyleSheet, Text,View, Button} from 'react-native'
+
+export default function WelcomeScreen() {
+  return (
+    <ImageBackground
+    source = { require("../assets/background.jpg") }
+    style = { styles.background }
+    >
+      <View style = {styles.logocontainer}>
+        <Image source={ require('../assets/bunny.png') } style = { styles.logo } />
+        <Text>Track Bunny and Plants everyday!</Text>
+      </View>
+      <View style = {styles.buttoncontainer}>
+        <Button title = "GetStart!" style = {styles.button}/>
+      </View>
+    </ImageBackground>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  background:{
+    flex:1,
+    justifyContent : 'center',
+    alignItems : 'center',
+  },
+  logocontainer:{
+    flex : 0.5,
+    position:'relative',
+    top: 100,
+    alignItems: 'center',
+  },
+  logo: {
+    width:200,
+    height:200,
+  },
+  buttoncontainer:{
+    flex:0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
