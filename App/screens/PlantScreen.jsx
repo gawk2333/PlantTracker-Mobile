de558863cc50d720d76d8e182dc141e44581ce93
Kang-Gao <64079938+gawk2333@users.noreply.github.com>
2021-12-22 15:47:49 +1300

    fixed reducer bugs and timeer errors

--- App/screens/PlantScreen.jsx
+++ App/screens/PlantScreen.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { StyleSheet, View, ScrollView, Button } from 'react-native'
 import { connect } from 'react-redux'
+import { editPlant } from '../../actions/plants'
 import EachPlant from '../components/EachPlant'
 
 
-const PlantScreen = ({navigation, plants, species}) => {
-  let plantWithWaterTime = plants.map(plant => {
-      plantSpecie = species.filter( specie => specie.Common === plant.SpecieCommon)
+const PlantScreen = ({navigation, plants, species, dispatch}) => {
+  let plantWithWaterTime = plants?.map(plant => {
+      let plantSpecie = species?.filter( specie => specie.Common === plant.SpecieCommon)
       plant.WaterFrequency = plantSpecie[0].WaterFrequency
       return plant
   })
+
+    const WaterHandler = (plant) => {
+      plant.lastWateredTime = Date.now()
+      dispatch(editPlant(plant))
+  }
+
   return (
     <View style = {styles.background}>
       <ScrollView contentContainerStyle={styles.plantlist} showsHorizontalScrollIndicator='false'>
           {plantWithWaterTime?.map(plant => {
-              return <EachPlant key={plant.PlantName} plant={plant} navigation={navigation}/>
+              return <EachPlant key={plant.PlantName} plant={plant} navigation={navigation} WaterHandler={WaterHandler}/>
           })}
         </ScrollView>
         <View style = { styles.addbutton }>
--- reducers/species.js
+++ reducers/species.js
@@ -11,13 +11,22 @@ const reducer = (state = InitialState , action) => {
       return [...state,action.species]
     case EDIT_SPECIE:
       const newState = state.filter(specie => specie.Common !== action.species.Common)
-      return [...newState,action.species]
+      return [...newState,action.species].sort(compare)
     case DELETE_SPECIE:
       return  state.filter(specie => specie.Common !== action.species.Common)
     case PURGE_SPECIE:
       return []
     default:
       return state;
+  }
+  function compare( a, b ) {
+    if ( a.Common < b.Common ){
+      return -1;
+    }
+    if ( a.Common > b.Common ){
+      return 1;
+    }
+    return 0;
   }
 }
 
